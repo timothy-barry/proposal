b139744cd57800afe824918fe6906fc258986fa6
Tim <tbarry2@andrew.cmu.edu>
2022-01-19 00:47:53 -0500

    paper idea 2

--- code/weighted_rank_dist.R
+++ code/weighted_rank_dist.R
@@ -1,17 +1,28 @@
-# The ranks are uniformly distributed (regardless of underlying distribution);
-# what about weighted ranks? Same deal.
+library(magrittr)
 
 B <- 1000
 n <- 10000
 x_grid <- seq(-2, 2, length.out = B - 1)
-# a <- dnorm(x = x_grid, mean = 0, sd = 2)
-a <- rep(1, B - 1)
+a <- dnorm(x = x_grid, mean = 0, sd = 1)
+# a <- c(rep(0, (B - 1)/3), rep(1, (B - 1)/3), rep(0, (B - 1)/3))
 
-ranks <- replicate(n = n, expr = {
+# compute the theoretical null distribution (in both unweighted and weighted cases)
+theoretical_unranked_dist <- seq(1, B)
+theoretical_ranked_dist <- 1 + cumsum(c(0, a))
+
+# compute the empirical null distribution 
+res <- replicate(n = n, expr = {
   samp <- rnorm(n = B)
   t_star <- samp[1]
   t_order <- sort(samp[2:B])
-  sum(a * (t_star >= t_order)) + 1
-})
+  unweight_rank <- sum(t_star >= t_order) + 1
+  weight_rank <- sum(a * (t_star >= t_order)) + 1
+  return(c(unweight_ranks = unweight_rank, weight_ranks = weight_rank))
+}) %>% t()
 
-hist(ranks)
+
+hist(res[,"weight_ranks"], breaks = 16)
+hist(res[,"unweight_ranks"], breaks = 16)
+
+mean(res[,"weight_ranks"] >= 250.3222)
+mean(res[,"unweight_ranks"] >= 900)
